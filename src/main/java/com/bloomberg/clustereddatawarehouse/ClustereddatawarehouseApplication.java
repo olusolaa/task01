b05c1e10c3b54ff2ddd0345e2ceb0aa508991b90
olusolaa <alaoolusolae@gmail.com>
2023-11-09 13:32:13 +0100

    CRE-001 feat: Initialize clustered data warehouse server

---
+++ src/main/java/com/bloomberg/clustereddatawarehouse/ClustereddatawarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.bloomberg.clustereddatawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClustereddatawarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClustereddatawarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bloomberg/clustereddatawarehouse/ClustereddatawarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomberg.clustereddatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClustereddatawarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
